6c4a772539e1d45e9bccb636418965b3f686ccc8
Sparty27 <nyshchyi.nazar@gmail.com>
2023-11-28 12:45:47 +0200

    Add project files.

---
+++ MINT_WebAPI/Controllers/BrandController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MINT_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
